6a1ed6327008c6b1a5eb28c695ae8fbee4ddb190
mudamudacoder <agrimsiwakoti5@gmail.com>
2024-10-21 21:06:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,4 @@ app.delete('/api/products/:id', async (req, res) => {
 });
 
 // Start the server and listen on a port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
